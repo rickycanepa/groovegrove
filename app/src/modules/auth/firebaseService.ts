777f898ad949e71524d663e98ca5bcb12f708794
Ricky Canepa <rickycanepa.rc@gmail.com>
2024-01-10 14:42:54 -0600

    firebase config added

--- app/src/modules/auth/firebaseService.ts
+++ app/src/modules/auth/firebaseService.ts
@@ -2,12 +2,3 @@ import ReactFirebaseUI from "firebaseui-react";
 
 const auth = getAuth();
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAsjh6LzpVsQI-7UyaaQW6mvyl2HaJSPck",
-    authDomain: "groove-grove.firebaseapp.com",
-    projectId: "groove-grove",
-    storageBucket: "groove-grove.appspot.com",
-    messagingSenderId: "353790456559",
-    appId: "1:353790456559:web:0ce17380c3ca240079e11a",
-    measurementId: "G-SFV2EZV6YP"
-};
