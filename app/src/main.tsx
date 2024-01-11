0292adeba40da54d78dcda0e55ed08d9bee7a611
Justin Sweeten <jtsweeten@gmail.com>
2024-01-11 11:51:24 -0600

    rebuilt docker image

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx'
 import Login from './components/login.tsx';
 import './index.css'
