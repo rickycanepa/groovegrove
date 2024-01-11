1b069e2686bddbd790de6fd822fe1f62f50a1c04
Ricky Canepa <rickycanepa.rc@gmail.com>
2024-01-11 13:08:29 -0600

    created auth component

---
+++ app/src/components/auth.tsx
@@ -0,0 +1,8 @@
+export const Auth = () => {
+    return (
+        <div>
+            <input placeholder="Email..."/>
+            <input placeholder="Password..."/>
+        </div>
+    )
+}
--- app/src/config/firebase.ts
+++ app/src/config/firebase.ts
@@ -20,4 +20,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+export const auth = getAuth(app)
