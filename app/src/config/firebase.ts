cac13fce5ef036b47d846d501e90dd0463f534b8
Justin Sweeten <jtsweeten@gmail.com>
2024-01-14 13:31:07 -0600

    firebaseConfig secrets have been moved to .env file

--- app/src/config/firebase.ts
+++ app/src/config/firebase.ts
@@ -1,23 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCuS0hlDdXNoJnTcrumwkVcoZC6CNpczJc",
-  authDomain: "groove-grove-3d74c.firebaseapp.com",
-  projectId: "groove-grove-3d74c",
-  storageBucket: "groove-grove-3d74c.appspot.com",
-  messagingSenderId: "478223878731",
-  appId: "1:478223878731:web:35de13c763686fafe51bce",
-  measurementId: "G-MLY84MMRYT"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
