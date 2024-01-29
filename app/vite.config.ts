388447a9d9eb6864e345f49a7e436e7011d360b1
Justin Sweeten <jtsweeten@gmail.com>
2024-01-29 14:17:09 -0600

    save point before attempting to create docker dev environment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://express-server:5000'
-    }
+  preview: {
+    port: 8080,
+    strictPort: true,
   },
-})
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+    proxy: {
+      '/api': 'http://express-server:5000',
+    },
+  },
+});
